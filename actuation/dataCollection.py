ea815cf96db0922943ea96437e439f6b245bddce
Lasse <lasse.kotz@outlook.com>
2023-01-23 17:45:38 +0100

    useless commit

--- actuation/dataCollection.py
+++ actuation/dataCollection.py
@@ -24,7 +24,7 @@ def main(motor1, ang_lim, camera):
         ext = '.jpg'
         imgname = 'picture%s%s' %(j, ext) #format image name string
         print(imgname)
-        camera.capture("Images", imgname)
+        camera.capture("/Images", imgname)
         
         time.sleep(1)
         j += 1
