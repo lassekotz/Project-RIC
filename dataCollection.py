4feaf74404d01857b1686a67d535b7d21488579e
Jonathan <ja125@live.se>
2023-01-26 13:27:31 +0100

    Bug fix

--- dataCollection.py
+++ dataCollection.py
@@ -2,30 +2,26 @@
     Script to collect data needed for ML
 """
 
-import sched
 import time
-import IMU.IMU as imu
+import ctypes
 import smbus2					#import SMBus module of I2C
 import math
 import actuation.dataCollection as actu
 import actuation.camera as cam
+from time import sleep
+
+imufunc = ctypes.CDLL("/IMU/build/libimu.so")
 
 
 
 bus = smbus2.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
 Device_Address = 0x68   # MPU6050 device address
-currAngle = imu.setupGyroTheta()
+currAngle = imufunc.setupGyroTheta()
 dt = 0.1 #Time step IMU
+imufunc.update_angle.argtypes = [ctypes.c_double]
 
-scheduler = sched.scheduler(time.time,
-                            time.sleep)
-
-
-imu_event = scheduler.enter(dt,1,
-    currAngle=imu.Update_angle(currAngle,dt))
-
-snap_event = scheduler.enter(1,10,cam.camera.capture("shit.jpg"))
-
-#Insert shitty Lasse event functions here 
-
-scheduler.run()
+while(True):
+    currAngle = imufunc.update_angle(currAngle,dt)
+    print(currAngle)
+    sleep(0.1)
+    
