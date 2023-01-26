7dc2547e870ffdc6328576bfbeb14bf049375837
Jonathan <ja125@live.se>
2023-01-26 14:40:51 +0100

    Bug fix

--- dataCollection.py
+++ dataCollection.py
@@ -16,7 +16,7 @@ bus = smbus2.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
 Device_Address = 0x68   # MPU6050 device address
 imu.MPU_Init()
 currAngle = imu.setupGyroTheta()
-t1 = datetime.datetime.now()
+time1 = datetime.datetime.now()
 #dt = 0.1 #Time step IMU
 camera = cam.Camera()
 camera.initialize()
@@ -37,12 +37,12 @@ while(True):
 
 def runA():
     while True:
-            t2 = datetime.datetime.now()
-            dt = (t2 - t1)
+            time2 = datetime.datetime.now()
+            dt = (time2 - time1)
             currAngle = imu.Update_angle(currAngle,dt.total_seconds())
     
             print(currAngle)
-            t1 = t2
+            time1 = time2
             sleep(0.1)
 
 def runB():
