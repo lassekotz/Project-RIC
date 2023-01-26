55b16230c576379c4a2f89421655e129f5a2a6af
Jonathan <ja125@live.se>
2023-01-26 14:38:21 +0100

    Threading!

--- dataCollection.py
+++ dataCollection.py
@@ -9,8 +9,7 @@ import math
 import actuation.camera as cam
 from time import sleep
 import datetime
-
-
+from threading import Thread
 
 
 bus = smbus2.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
@@ -22,16 +21,41 @@ t1 = datetime.datetime.now()
 camera = cam.Camera()
 camera.initialize()
 i = 0
+"""
 while(True):
     t2 = datetime.datetime.now()
     dt = (t2 - t1)
     currAngle = imu.Update_angle(currAngle,dt.total_seconds())
     
-    #camera.capture_image(i , resolution = (960, 540))
+    #
     
     print(currAngle)
     t1 = t2
     sleep(0.1)
     i += 1
-    
+   """ 
 
+def runA():
+    while True:
+            t2 = datetime.datetime.now()
+            dt = (t2 - t1)
+            currAngle = imu.Update_angle(currAngle,dt.total_seconds())
+    
+            print(currAngle)
+            t1 = t2
+            sleep(0.1)
+
+def runB():
+    while True:
+        camera.capture_image(i , resolution = (960, 540))
+        i +=1
+
+if __name__ == "__main__":
+    t1 = Thread(target = runA)
+    t2 = Thread(target = runB)
+    t1.setDaemon(True)
+    t2.setDaemon(True)
+    t1.start()
+    t2.start()
+    while True:
+        pass
