2ef414926e158c75bccfdca43003f6c66a8110ea
Jonathan <ja125@live.se>
2023-01-26 13:58:24 +0100

    Previous commit was not bug fix :(

--- dataCollection.py
+++ dataCollection.py
@@ -3,25 +3,32 @@
 """
 
 import time
-import ctypes
+import IMU.IMU as imu
 import smbus2					#import SMBus module of I2C
 import math
 import actuation.dataCollection as actu
 import actuation.camera as cam
 from time import sleep
+import datetime
 
-imufunc = ctypes.CDLL("/IMU/build/libimu.so")
 
 
 
 bus = smbus2.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
 Device_Address = 0x68   # MPU6050 device address
-currAngle = imufunc.setupGyroTheta()
-dt = 0.1 #Time step IMU
-imufunc.update_angle.argtypes = [ctypes.c_double]
+imu.MPU_Init()
+currAngle = imu.setupGyroTheta()
+t1 = datetime.datetime.now()
+#dt = 0.1 #Time step IMU
+
 
 while(True):
-    currAngle = imufunc.update_angle(currAngle,dt)
+    t2 = datetime.datetime.now()
+    dt = (t2 - t1)
+    currAngle = imu.Update_angle(currAngle,dt.total_seconds())
+    
     print(currAngle)
+    t1 = t2
     sleep(0.1)
     
+
