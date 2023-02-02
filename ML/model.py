2f1637b189f66ba0e63d43e2a63cdf3ac32c6858
Lasse <lasse.kotz@outlook.com>
2023-02-02 15:54:43 +0100

    Updated model.py and included transform helper-functions

--- ML/model.py
+++ ML/model.py
@@ -3,7 +3,10 @@ from torch import nn
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 from torchvision.transforms import Compose, ToTensor
+from torchvision import transforms
 import os
+import matplotlib.pyplot as plt
+
 
 class ImagesDataset(Dataset):
 
@@ -78,25 +81,87 @@ class ImagesDataset(Dataset):
         id_index = [path.stem for (path, _) in self._samples].index(id_)
         return self[id_index]
 
+def display_image(axis, image_tensor):
+    """Display a tensor as an image
 
+    Args:
+        axis (pyplot axis)
+        image_tensor (torch.Tensor): tensor with shape (num_channels=3, width, heigth)
+    """
+
+    # See hint above
+    if not isinstance(image_tensor, torch.Tensor):
+        raise TypeError("The `display_image` function expects a `torch.Tensor` " +
+                        "use the `ToTensor` transformation to convert the images to tensors.")
+
+    # The imshow commands expects a `numpy array` with shape (3, width, height)
+    # We rearrange the dimensions with `permute` and then convert it to `numpy`
+    image_data = image_tensor.permute(1, 2, 0).numpy()
+    height, width, _ = image_data.shape
+    axis.imshow(image_data)
+    axis.set_xlim(0, width)
+    # By convention when working with images, the origin is at the top left corner.
+    # Therefore, we switch the order of the y limits.
+    axis.set_ylim(height, 0)
+
+def compare_transforms(transformations, index):
+    """Visually compare transformations side by side.
+    Takes a list of DogsCatsData datasets with different compositions of transformations.
+    It then display the `index`th image of the dataset for each transformed dataset in the list.
+
+    Example usage:
+        compare_transforms([dataset_with_transform_1, dataset_with_transform_2], 0)
+
+    Args:
+        transformations (list(DogsCatsData)): list of dataset instances with different transformations
+        index (int): Index of the sample in the dataset you wish to compare.
+    """
+
+    # Here we combine two functions from basic python to validate the input to the function:
+    # - `all` takes an iterable (something we can loop over, e.g. a list) of booleans
+    #    and returns True if every element is True, otherwise it returns False.
+    # - `isinstance` checks whether a variable is an instance of a particular type (class)
+    if not all(isinstance(transf, Dataset) for transf in transformations):
+        raise TypeError("All elements in the `transformations` list need to be of type Dataset")
+
+    num_transformations = len(transformations)
+    fig, axes = plt.subplots(1, num_transformations)
+
+    # This is just a hack to make sure that `axes` is a list of the same length as `transformations`.
+    # If we only have one element in the list, `plt.subplots` will not create a list of a single axis
+    # but rather just an axis without a list.
+    if num_transformations == 1:
+        axes = [axes]
+
+    for counter, (axis, transf) in enumerate(zip(axes, transformations)):
+        axis.set_title(f"transf: {counter}")
+        image_tensor = transf[index][0]
+        display_image(axis, image_tensor)
+
+    plt.show()
 
 class LinearModel(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, input_size):
         super().__init__()
 
+
         self.layer1 = nn.Linear(in_features=input_size, out_features= 1)
-        self.loss_fn = nn.MSELoss()
-        #self.layer2
-        #self.layer3
+        #self.relu1 = nn.ReLU()
+        #self.layer2 = nn.Linear(in_features=1000, out_features=1)
+        self.loss_fn = nn.L1Loss()
+        #self.loss_fn = nn.MSELoss()
 
     def forward(self, x):
         #x = torch.flatten(x)
         a1 = self.layer1(x)
-        #o = self.out(a1)
+        #o1 = self.relu1(a1)
+        #o = self.layer2(o1)
 
-        return a1
+        o = a1
 
-def train_one_epoch(print_freq):
+        return o
+
+def train_one_epoch(print_freq, train_dataloader, model, optim):
     cumulative_loss = 0
     current_loss = 0
 
@@ -104,34 +169,72 @@ def train_one_epoch(print_freq):
         inputs, labels = datapoint
         inputs = torch.flatten(inputs, start_dim=1, end_dim=-1)
 
-        labels = labels.float()
-
         preds = model.forward(inputs)
-        loss = model.loss_fn(preds, labels)
+        loss = model.loss_fn(preds, labels.float().unsqueeze(1))
         loss.backward()
+        optim.step()
         cumulative_loss += loss.item()
 
         if (i%print_freq == 0):
             current_loss = cumulative_loss/print_freq
             print('batch: {} loss: {}'.format(i+1, current_loss))
 
-
             cumulative_loss = 0
 
-    return None
+    return current_loss
 
 def train_full_epochs(nr_of_epochs, print_freq):
+    losslist = []
     for j in range(nr_of_epochs):
         print("======================")
         print("EPOCH {}".format(j+1))
-        train_one_epoch(print_freq)
+        latest_loss = train_one_epoch(print_freq)
+        losslist.append(latest_loss)
 
-data_transforms = Compose([ToTensor()])
-train_dataset = ImagesDataset('./Train', data_transforms)
+    return losslist
+
+train_path = './Train'
+
+transform_random = transforms.Compose([
+        transforms.RandomResizedCrop(64),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+transform_center = transforms.Compose([
+        transforms.CenterCrop(64),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+transform_resize_greyscale_normalize = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.Grayscale(3),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+all_transforms = [ImagesDataset(train_path, transform_resize_greyscale_normalize), ImagesDataset(train_path, transform_random), ImagesDataset(train_path, transform_center), ImagesDataset(train_path, Compose([ToTensor()]))]
+compare_transforms(all_transforms, 1)
+
+
+
+'''
 train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 input_size = torch.numel(train_dataset.__getitem__(0)[0])
 
 model = LinearModel()
+optim = torch.optim.Adam(model.parameters(), lr = 0.00000001)
 
-#train_one_epoch(1)
-train_full_epochs(5, 2)
+## TRAINING
+
+epochs = 50
+print_freq = 1
+losslist = train_full_epochs(epochs, print_freq)
+plt.plot(losslist)
+plt.show()
+
+test_x = train_dataset.__getitem__(1)[0].flatten()
+test_pred = model.forward(test_x)
+test_label = torch.Tensor(train_dataset.__getitem__(1)[])
+'''
