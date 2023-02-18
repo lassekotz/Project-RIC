d3699e9b077ac5d112dc60b79b4b76eb12ca545f
Jonathan <ja125@live.se>
2023-02-18 19:37:49 +0100

    Updated motorcontroller

--- motorControl.c
+++ motorControl.c
@@ -2,13 +2,19 @@
 #include <wiringPi.h>
 #include <stdlib.h>
 
-// Declaring pins
+// Declaring pins for motors
 const int en1 = 12; //Change these 
 const int in1 = 23; 
 const int in2 = 24;
 const int in3 = 8;
 const int in4 = 7;
-const int en2 = 13; 
+const int en2 = 13;
+// Declaring pins for encoders
+const int chA1 = 26; //Change these 
+const int chB1 = 19;
+const int chA2 = 21;  
+const int chB2 = 20; 
+int pos = 0;
 
 wiringPiSetupGpio(); //Setup and use defult pin numbering
 
@@ -18,6 +24,21 @@ pinMode(in1,OUTPUT);
 pinMode(in2,OUTPUT);
 pinMode(in3,OUTPUT);
 pinMode(in4,OUTPUT);
+
+
+const double alpha = 360.0/3.0/231.0; // Relative angle per encoder step
+pinMode(chA1,INPUT);
+pinMode(chB1,INPUT);
+
+void readEncoder(){
+    int b = digitalRead(chB1);
+    if(b >0){
+        pos++;
+    }
+    else{
+        pos--;
+    }
+}
 
 
 
@@ -49,4 +70,9 @@ int accuateMotor(int power1,int dir1,int power2,int dir2){
 
 }
 
+int main(){
+    u_int currT = millis()
+    wiringPiISR(chA1,INT_EDGE_RISING, &readEncoder);
 
+}
+n 
