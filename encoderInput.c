f7388df58bdc2173d2995214cc934d6ea31f4882
lassekotz <lasse.kotz@outlook.com>
2023-02-18 19:41:11 +0100

    Handling encoder inputs

--- encoderInput.c
+++ encoderInput.c
@@ -3,34 +3,44 @@
 #include <wiringPi.h>
 #include <stdlib.h>
 
-wiringPiSetupGpio(); //Setup and use defult pin numbering
+
 
 // Declare pins
-const int chA1 = 1; //Change these 
-const int chB1 = 1;
-const int chA2 = 1;  
-const int chB2 = 1; 
-int pos = 0;
+const int chA1 = 21; //Change these 
+const int chB1 = 20;
+//const int chA2 = 1;  
+//const int chB2 = 1; 
+double pos = 0;
 
 const double alpha = 360.0/3.0/231.0;
-pinMode(chA1,INPUT);
-pinMode(chB1,INPUT);
 
-void readEncoder(){
+
+void readEncoder(){ 
     int b = digitalRead(chB1);
     if(b >0){
-        pos++;
+        pos= pos+alpha;
     }
     else{
-        pos--;
+        pos = pos-alpha;
     }
+    printf("Current angle is %f",pos);
 }
 
 int main(){
-
-    wiringPiISR(chA1,INT_EDGE_RISING, &readEncoder);
+    wiringPiSetupGpio(); //Setup and use defult pin numbering
+    pinMode(chA1,INPUT);
+    pinMode(chB1,INPUT);
+    printf("The pos is: %f",pos);
+    wiringPiISR(chA1,INT_EDGE_RISING, &readEncoder); //Hardware interupt on encoder pin
+    while(1){
+        delay(500);
+        printf("\n");
+    }
     // wiringPiISR (0, INT_EDGE_FALLING, &myInterrupt0) ;
     // int waitForInterrupt (int pin, int timeOut) ;
     return 0;
 }
+
+//gcc encoderInput.c -o EN -lwiringPi -lm
+
 
