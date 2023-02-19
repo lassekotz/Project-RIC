2977ed365d7c59d5dd714e22d838a132327f601e
Jonathan <ja125@live.se>
2023-02-19 10:07:54 +0100

    Updated motor drivers

--- MotorDriver/motorControl.c
+++ MotorDriver/motorControl.c
@@ -14,32 +14,36 @@ const int chA1 = 26; //Change these
 const int chB1 = 19;
 const int chA2 = 21;  
 const int chB2 = 20; 
-int pos = 0;
+double pos1 = 0; //Keeps track of current angle 
+double pos2 = 0;
 
-wiringPiSetupGpio(); //Setup and use defult pin numbering
 
-pinMode(en1,PWM_OUTPUT);
-pinMode(en2,PWM_OUTPUT);
-pinMode(in1,OUTPUT);
-pinMode(in2,OUTPUT);
-pinMode(in3,OUTPUT);
-pinMode(in4,OUTPUT);
 
 
 const double alpha = 360.0/3.0/231.0; // Relative angle per encoder step
-pinMode(chA1,INPUT);
-pinMode(chB1,INPUT);
 
-void readEncoder(){
+
+void readEncoder1(){ 
     int b = digitalRead(chB1);
     if(b >0){
-        pos++;
+        pos1 = pos1+alpha;
     }
     else{
-        pos--;
+        pos1 = pos1-alpha;
     }
+    printf("Current angle is %f",pos);
 }
 
+void readEncoder2(){ 
+    int b = digitalRead(chB2);
+    if(b >0){
+        pos2 = pos2+alpha;
+    }
+    else{
+        pos2 = pos2-alpha;
+    }
+    printf("Current angle 2 is %f",pos);
+}
 
 
 int accuateMotor(int power1,int dir1,int power2,int dir2){ 
@@ -71,8 +75,26 @@ int accuateMotor(int power1,int dir1,int power2,int dir2){
 }
 
 int main(){
+    wiringPiSetupGpio(); //Setup and use defult pin numbering
+
+    //Setting pinmodes for motor pins
+    pinMode(en1,PWM_OUTPUT);
+    pinMode(en2,PWM_OUTPUT);
+    pinMode(in1,OUTPUT);
+    pinMode(in2,OUTPUT);
+    pinMode(in3,OUTPUT);
+    pinMode(in4,OUTPUT);
+    //Pins for encoder 
+    pinMode(chA1,INPUT);
+    pinMode(chB1,INPUT);
+
+
     u_int currT = millis()
-    wiringPiISR(chA1,INT_EDGE_RISING, &readEncoder);
+    wiringPiISR(chA1,INT_EDGE_RISING, &readEncoder1); // Hardware interupt for encoder 1
+    wiringPiISR(chA2,INT_EDGE_RISING, &readEncoder2);
+    while(1){
+        delay(500);
+        printf("\n");
+    }
 
 }
-n 
