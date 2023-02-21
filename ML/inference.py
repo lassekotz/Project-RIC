5f965d6138539e31cf7fd479c1d2656d854d43ac
Lasse <lasse.kotz@outlook.com>
2023-02-21 16:29:27 +0100

    Fix and added inference.py

---
+++ ML/inference.py
@@ -0,0 +1,29 @@
+import numpy as np
+import tensorflow as tf
+import time
+
+def run_inference():
+
+    interpreter = tf.lite.Interpreter(model_path="trained_models/vgg16/vgg16.tflite")
+    interpreter.allocate_tensors()
+
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Test the model on random input data.
+    input_shape = input_details[0]['shape']
+    input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    interpreter.invoke()
+
+    # The function `get_tensor()` returns a copy of the tensor data.
+    # Use `tensor()` in order to get a pointer to the tensor.
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    print(output_data)
+    print(len(output_data))
+
+t0 = time.time()
+run_inference()
+print("Elapsed time: " + str(time.time() - t0))
