06bfc48bf9ae76547118690f22ddcfacd64d6d99
Lasse <lasse.kotz@outlook.com>
2023-03-01 16:11:46 +0100

    added error distribution plot

--- ML/analyze.py
+++ ML/analyze.py
@@ -9,7 +9,6 @@ def view_label_distr(filepath, bins = 10):
             vals.append(float(line.replace('\n', '')))
 
     vals_np = np.array(vals)
-    hist = np.histogram(vals_np)
     mean = np.mean(vals_np)
     plt.axvline(x=mean, color='r', label='axvline - full height')
     plt.hist(vals_np, bins = bins)
@@ -18,9 +17,10 @@ def view_label_distr(filepath, bins = 10):
     plt.title('Distribution of angles in dataset ' + str(filepath))
     plt.show()
 
-def plot_error_distr():
-    #TODO: Implement an error distribution tool
-    pass
+def plot_error_distr(errors_list, bins = 20):
+    errors_np = np.array(errors_list)
+    plt.hist(errors_np, bins)
 
-filepath = './Data/BigDataset/labels.txt'
-view_label_distr(filepath, bins = 15)
+    plt.title('Error distribution')
+    plt.show()
+
