40d971939616dae72616881fdabf40c3ee19b7ba
Lasse <lasse.kotz@outlook.com>
2023-03-01 17:59:13 +0100

    cleanup etc.

--- ML/analyze.py
+++ ML/analyze.py
@@ -19,8 +19,11 @@ def view_label_distr(filepath, bins = 10):
 
 def plot_error_distr(errors_list, bins = 20):
     errors_np = np.array(errors_list)
-    plt.hist(errors_np, bins)
 
-    plt.title('Error distribution')
+    fix, (ax1, ax2) = plt.subplots(1, 2)
+
+    ax1.hist(errors_np, bins)
+    ax1.set_title('Error distribution')
+    ax2.plot(errors_list)
+    ax2.set_title('Errors over time')
     plt.show()
-
