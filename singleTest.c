85daa247b45143d45b730298589295284ba50ecc
Jonathan <ja125@live.se>
2023-03-13 13:52:47 +0100

    New test case

---
+++ singleTest.c
@@ -0,0 +1,59 @@
+// Include standard libraries  
+#include <stdio.h>
+#include <wiringPi.h>
+#include <stdlib.h>
+#include <math.h>
+// Include project libraries 
+#include "motorControl.h"
+#include "pidMotor.h"
+#include "call_IMU.h"
+#define EVER ;;
+// This tests the multiproccessing enviroment
+
+float curTheta;
+float u = 0;
+int desPower;
+int dir;
+
+
+void setup(){
+    wiringPiSetupGpio(); //Setup and use defult pin numbering
+    MPU6050_Init();
+    
+    initMotorPins(); //Initializes pins and hardware interupts for motors
+    initRegParam(15.545755616, 0, 20.4835, -0.0431, -0.0464);
+    setupFirstValue();
+}
+
+int main(){
+    setup();
+    piThreadCreate (IMU_thread);
+    piThreadCreate(PID_thread);
+    piThreadCreate(motor_thread);
+    for(EVER){
+
+        //Update IMU
+        curTheta = update_angle(1);
+        
+        //Calc u 
+        u =angleController(curTheta,0.0, 0.0);
+
+        desPower = fabs(u*1024.0/12.0);
+        if(u<0){
+            dir = 0; //Maybe the other way? Test and see 
+        }
+        else{
+            dir = 1;
+        }
+
+        accuateMotor(desPower,dir,desPower,dir);
+
+        //Check for failure
+        if(abs(curTheta)>15){
+            accuateMotor(0,1,0,1);
+            exit(1);
+        }
+        delay(10);
+    }
+}
+
