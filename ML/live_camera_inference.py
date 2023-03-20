a37ab52f84bd58acba88bb6593cfcd5101344841
Lasse <lasse.kotz@outlook.com>
2023-03-20 23:48:12 +0100

    unfinished bizz

---
+++ ML/live_camera_inference.py
@@ -0,0 +1,45 @@
+import numpy as np
+import time
+import tflite_runtime.interpreter as tflite
+import sys
+import picamera
+
+
+def inference_step():
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Test the model on input data
+    input_shape = input_details[0]['shape']
+    print(input_shape)
+    input_data = np.array(np.random.random_sample(input_shape))
+
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    # The function 'get_tetnsor()' returns a copy of the tensor data.
+    # Use 'tensor()' in order to get a pointer to the tensor.
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+
+def initialize():
+    model = str(sys.argv[1])
+    print("CAMERA INITIALIZING...")
+    camera = picamera.PiCamera()
+    camera.resolution = (128, 128)
+    camera.framerate = 32
+    rawCapture = PiRGBArray(camera, size=camera.resolution)
+    print("CAMERA INITIALIZED!")
+    print("LOADING MODEL...")
+    interpreter = tflite.Interpreter(model_path="trained_models/" + model + "/" + model + ".tflite")
+    interpreter.allocate_tensors()
+    print("MODEL LOADED!")
+
+    return camera, interpreter, rawCapture
+
+def main():
+    camera, interpreter, rawCapture = initialize()
+
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):)
+        image = frame.array
+
+        #TODO: wrap up...
--- ML/tflite_conversion.py
+++ ML/tflite_conversion.py
@@ -10,7 +10,10 @@ def save_and_convert_model(model_name, model, dummy_input, input_names, output_n
     if not os.path.exists('./trained_models/' + str(model_name)):
         os.mkdir('./trained_models/' + str(model_name))
     path = './trained_models/' + str(model_name) + "/" + str(model_name)
-    torch.onnx.export(model, dummy_input, path + '.onnx', verbose=True, input_names=input_names,
+
+    #TODO: Add compatibility for VGG. https://github.com/pytorch/pytorch/issues/5310
+
+    torch.onnx.export(model, dummy_input, path + '.onnx', verbose=False, input_names=input_names,
                       output_names=output_names)
     onnx_model = onnx.load(path + ".onnx")
     onnx.checker.check_model(onnx_model)
