33d75ab6a7a8253e91ba1955f2073318eb28a178
Jonathan <ja125@live.se>
2023-03-21 12:48:51 +0100

    Updated regulator to include filter

--- MotorDriver/pidMotor.h
+++ MotorDriver/pidMotor.h
@@ -4,7 +4,7 @@
 // Functions
 float angleController(float angle,float v, float vref);
 void initRegParam(float Kp, float Ki, float Kd, float Kpv, float Kiv);
- 
+float motorRegulator(float v1, float v2,float diffRef);
 
 #endif
 
