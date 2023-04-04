4b6898d38e8b562ff7ddae66059cbc1109faf6ce
Jonathan Almgren <jonathan.almgren@knowit.se>
2023-04-04 17:21:31 +0200

    improved uint8 quantization

--- ML/tflite_conversion.py
+++ ML/tflite_conversion.py
@@ -29,18 +29,35 @@ def save_and_convert_model(model_name, model, dummy_input, input_names, output_n
     tflite_model = converter.convert()
 
     def representative_data_gen():
-        for input_value in tf.data.Dataset.from_tensor_slices(dummy_input.cpu().numpy()).batch(1).take(2):
-            yield [input_value]
+        #dataset_list = tf.data.Dataset.list_files('C:/Users/jonalm3/Project-RIC/ML/Data/BigDataset/images' + "/*/*")
+        for i in range(500):
+            #image = next(iter(dataset_list))
+            image = tf.io.read_file("C:/Users/jonalm3/Project-RIC/ML/Data/BigDataset/images/" + str(i) + ".jpg")
+            image = tf.io.decode_jpeg(image, channels=3)
+            image = tf.image.resize(image, [128, 128])
+            image = tf.cast(image / 255., tf.float32)
+            image = tf.expand_dims(image, 0)
+            yield [image]
+        #for input_value in tf.data.Dataset.from_tensor_slices(dummy_input.cpu().numpy()).batch(1).take(2):
+            #yield [input_value]
 
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
     converter.representative_dataset = representative_data_gen
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+    converter.target_spec.supported_types = [tf.int8]
+    converter.inference_input_type = tf.uint8
+    converter.inference_output_type = tf.uint8
 
     tflite_model_quant = converter.convert()
 
     interpreter = tf.lite.Interpreter(model_content=tflite_model_quant)
+    print(interpreter.get_input_details())
     input_type = interpreter.get_input_details()[0]['dtype']
+    output_type = interpreter.get_output_details()[0]['dtype']
     print("input: ", input_type)
-    # TODO: modify quantization to int8
+    print("output: ", output_type)
+
+    # TODO: modify quantization to full int8
 
     # Save the model
 
