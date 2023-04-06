c07de09707ad327e689c1f6fd9856c601f54b34d
Lasse <lasse.kotz@outlook.com>
2023-04-06 11:15:25 +0200

    Semi-functioning TPU-inf with 2ms @500Hz sample-rate

--- ML/aarch64_inf.py
+++ ML/aarch64_inf.py
@@ -3,13 +3,13 @@ import time
 import tflite_runtime.interpreter as tflite
 import sys
 import cv2
+from PIL import Image
 from pycoral.utils import edgetpu
 from pycoral.utils import dataset
 from pycoral.adapters import common
 from pycoral.adapters import classify
 
 
-# from picamera.array import PiRGBArray
 
 
 def initialize(resolution=128):
@@ -21,26 +21,28 @@ def initialize(resolution=128):
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, resolution)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution)
-    cap.set(cv2.CAP_PROP_FPS, 50)
+    cap.set(cv2.CAP_PROP_FPS, 100)
 
     print("CAMERA INITIALIZED!")
     print("LOADING MODEL...")
-    interpreter = edgetpu.make_interpreter(model_path_or_content="trained_models/" + model + "/" + model + ".tflite")
+    interpreter = edgetpu.make_interpreter(model_path_or_content="trained_models/" + model + "/" + model + "_full_int_quant_edgetpu.tflite")
     interpreter.allocate_tensors()
+    #if common.input_details(interpreter, 'dtype') != np.uint8:
+	    #raise ValueError("Only np uint8 is supported")
+    size = common.input_size(interpreter)
+    print(common.input_details(interpreter, 'quantization_parameters'))
     print("MODEL LOADED!")
 
     return cap, interpreter
 
 
 def inference_step(interpreter, input_data, input_details, output_details):
-    # Test the model on input data
-    common.set_input(interpreter, input_data)
+    common.set_input(interpreter, input_data.astype(np.uint8))
+    ti = time.time()
     interpreter.invoke()
-    # The function 'get_tensor()' returns a copy of the tensor data.
-    # Use 'tensor()' in order to get a pointer to the tensor.
-    output_data = interpreter.get_tensor(output_details[0]['index'])
+    print(classify.get_classes(interpreter))
 
-    return output_data
+    return 1
 
 
 def main(write_to_disk = False):
@@ -59,18 +61,18 @@ def main(write_to_disk = False):
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-        if not ret:
-            raise RuntimeError("failed to read frame")
+        #if not ret:
+        #    raise RuntimeError("failed to read frame")
         image = image[:, :, [2, 1, 0]]
-        image = image/255
-        input_data = np.array(image, dtype=np.float32)
+        #image = image/255.0
+        input_data = np.array(image, dtype=np.uint8)
         input_data = np.expand_dims(input_data, axis=0)
-        input_data = np.swapaxes(input_data, 1, 3)
+        #print(input_data)
 
-        pred = inference_step(interpreter, input_data, input_details, output_details)
-        tTot = tTot + (time.time() - tt)
+        pred = inference_step(interpreter, input_data.astype(np.int8), input_details, output_details)
+        #tTot = tTot + (time.time() - tt)
         #pred = a*(pred) + (1-a)*pred
-        # print(f'{pred[0][0]:.2f}' + "\n")
+        print(f'{pred:.2f}' + "\n")
         #p_prev = pred
         #
         #
@@ -79,7 +81,7 @@ def main(write_to_disk = False):
         #
 
         if write_to_disk:
-            f.write(str(pred[0][0]) + "\n")
+            f.write(str(pred) + "\n")
         if i%100 == 0:
             print(f'{1000*tTot/i}' + " ms/iter")
             tTot = 0
