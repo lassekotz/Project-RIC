aad6fa4793ce1fbccf40e37e2453e3a39ab96d4a
Jonathan <ja125@live.se>
2023-04-28 10:46:53 +0200

    Updated shared mem

--- SharedMem/ReadFloat.cpp
+++ SharedMem/ReadFloat.cpp
@@ -3,28 +3,11 @@
 #include <unistd.h>
 #include <iostream>
 
-// Define a spin lock
-class SpinLock {
-private:
-    volatile bool _lock;
 
-public:
-    SpinLock() : _lock(false) {}
-
-    void acquire() {
-        while (__sync_lock_test_and_set(&_lock, true))
-            while (_lock)
-                asm volatile("pause");
-    }
-
-    void release() {
-        __sync_lock_release(&_lock);
-    }
-};
 
 int main() {
     // Open the named shared memory region
-    int shared_memory_fd = shm_open("/MySharedMemory", O_RDONLY, 0666);
+    int shared_memory_fd = shm_open("currAngle", O_RDONLY, 0666);
     if (shared_memory_fd == -1) {
         std::cerr << "Failed to open shared memory object\n";
         return 1;
@@ -39,19 +22,15 @@ int main() {
         return 1;
     }
 
-    // Define a spin lock
-    SpinLock spin_lock;
 
     // Continuously read the float value from shared memory
     while (true) {
-        // Acquire the spin lock before accessing the shared memory
-        spin_lock.acquire();
+
 
         // Read the binary data from shared memory and convert it to a float
         float shared_float = *((float*)shared_memory_ptr);
 
-        // Release the spin lock after accessing the shared memory
-        spin_lock.release();
+
 
         // Print the float value
         std::cout << "Shared float value: " << shared_float << std::endl;
--- SharedMem/UpdateFloat.py
+++ SharedMem/UpdateFloat.py
@@ -3,50 +3,29 @@ import struct
 import time
 import ctypes
 import random
+import os
+from multiprocessing import shared_memory
 
 # Define the size of the shared memory region
 SHARED_MEMORY_SIZE = 4
 
-# Create a named shared memory region
-shared_memory = mmap.mmap(-1, SHARED_MEMORY_SIZE, "MySharedMemory")
+
+shared = shared_memory.SharedMemory("currAngle",create=True, size=SHARED_MEMORY_SIZE)
 
 # Define a ctypes structure to pack the float value in binary format
 class FloatStruct(ctypes.Structure):
     _fields_ = [("value", ctypes.c_float)]
-
-# Create a ctypes instance of the FloatStruct
+    
 float_struct = FloatStruct()
+i = 0
 
-# Define a spin lock
-class SpinLock:
-    def __init__(self):
-        self._lock = False
-
-    def acquire(self):
-        while self._lock:
-            pass
-        self._lock = True
-
-    def release(self):
-        self._lock = False
-
-spin_lock = SpinLock()
-
-# Loop to write the float value to shared memory
 while True:
-    # Update the value in the ctypes struct
-    float_struct.value = random.random()
-
-    # Acquire the spin lock before accessing the shared memory
-    spin_lock.acquire()
-
-    # Pack the struct into binary format and write it to the shared memory region
-    shared_memory.seek(0)
-    shared_memory.write(ctypes.string_at(ctypes.byref(float_struct), ctypes.sizeof(float_struct)))
-
-    # Release the spin lock after accessing the shared memory
-    spin_lock.release()
-
-    # Wait for a short time before updating again
-    time.sleep(0.1)
-
+    float_struct.value = i
+    
+    shared.buf[:4] = ctypes.string_at(ctypes.byref(float_struct), ctypes.sizeof(float_struct))
+    
+    i += 1
+    if(i%10 == 0):
+        print(i)
+        
+    time.sleep(1)
